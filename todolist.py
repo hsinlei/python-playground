807b19f7d36ef4863587946ad6191b050475abd9
Charlene Wang <charlene.wang@brown.edu>
2024-01-04 15:14:47 +0000

    a better to do list

--- todolist.py
+++ todolist.py
@@ -14,11 +14,19 @@ while True:
     printList()
   elif menu == "add":
     item = input("What do you want to add to the list?: ")
-    todoList.append(item)
+    if item not in todoList:
+      todoList.append(item)
   elif menu == "edit":
-    item = input("Which item has you completed? ")
-    if item in todoList:
-      todoList.remove(item)
+    removal = input("Do you want to remove everything from the list?")
+    if removal == "yes":
+      todoList = []
     else:
-      print(f"{item} was not in the list")
+      item = input("Which item has you completed? ")
+      if item in todoList:
+        confirm = input(f"Are you sure you want to remove {item}? ")
+        if confirm == "yes":
+          todoList.remove(item)
+      else:
+        print(f"{item} was not in the list")
+
   printList()
