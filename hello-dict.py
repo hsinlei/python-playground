ef2b55fee11e8b5b4757043cfc3a0904fc83e159
Charlene Wang <charlene.wang@brown.edu>
2024-01-09 14:20:26 +0000

    moke beast

--- hello-dict.py
+++ hello-dict.py
@@ -1,8 +1,14 @@
-website = {"name": None, "url": None, "desc": None, "rating": None}
+moke_beast = {
+  "name": None,
+  "type": None,
+  "special move": None,
+  "starting HP": None,
+  "starting MP": None
+}
 
-for name in website.keys():
-  website[name] = input(f"{name}: ")
+for name, value in moke_beast.items():
+  moke_beast[name] = input(f"Input your beast's {name} >  ").strip().title()
 
-print()
-for name, value in website.items():
-  print(f"{name}: {value}")
+print(
+  f"Your beast is called {moke_beast['name']}. It is an {moke_beast['type']} beast with a special move of {moke_beast['special move']}"
+)
