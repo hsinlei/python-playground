924535c4df91e37da76712e537b834e62f31c858
Charlene Wang <charlene.wang@brown.edu>
2024-01-10 13:21:39 +0000

    a new todo list

--- todolist.py
+++ todolist.py
@@ -1,32 +1,48 @@
-todoList = []
+todo = []
 
 
-def printList():
-  print()
-  for item in todoList:
-    print(item)
-  print()
+def prettyPrint(list, prio):
+  count = 0
+  for row in list:
+    print(f"\n{count} : ")
+    for item in row:
+      if prio == "all":
+        print(f"{item:^10}", end=' | ')
+      elif prio == row[2]:
+        print(f"{item:^10}", end=' | ')
+    count += 1
 
 
 while True:
-  menu = input("view, add, or edit their list?:")
-  if menu == "view":
-    printList()
-  elif menu == "add":
-    item = input("What do you want to add to the list?: ")
-    if item not in todoList:
-      todoList.append(item)
-  elif menu == "edit":
-    removal = input("Do you want to remove everything from the list?")
-    if removal == "yes":
-      todoList = []
-    else:
-      item = input("Which item has you completed? ")
-      if item in todoList:
-        confirm = input(f"Are you sure you want to remove {item}? ")
-        if confirm == "yes":
-          todoList.remove(item)
-      else:
-        print(f"{item} was not in the list")
+  menu = input("\nDo you want to add, view, remove, or edit a to do? > ")
 
-  printList()
+  if menu.strip().lower() == "add":
+    todolist = input("What is the task? > ")
+    duedate = input("When is it due by? > ")
+    priority = input("What is the priority? > ")
+    row = [todolist, duedate, priority]
+    todo.append(row)
+    print("Thanks, this task has been added.")
+
+  elif menu.strip().lower() == "view":
+    view_options = input("\nDo you want to view all or view priority?")
+    if view_options.strip().lower() == "all":
+      prettyPrint(todo, "all")
+    else:
+      view_prio = input("Which priority do you want to view? > ")
+      choose = view_prio.strip().lower()
+      prettyPrint(todo, choose)
+
+  elif menu.strip().lower() == "edit":
+    prettyPrint(todo, "all")
+    a_option = int(input("Which to do do you want to edit?"))
+    todolist = input("New task name > ")
+    duedate = input("New due date > ")
+    priority = input("New priority > ")
+    todo[a_option] = [todolist, duedate, priority]
+    prettyPrint(todo, "all")
+  elif menu.strip().lower() == "remove":
+    prettyPrint(todo, "all")
+    remove_option = int(input("Which to do do you want to remove?"))
+    todo.remove(todo[remove_option])
+    prettyPrint(todo, "all")
