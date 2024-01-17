24a38c44e96c5440da0129b295dbe7f5a14bc0d0
Charlene Wang <charlene.wang@brown.edu>
2024-01-17 20:22:33 +0000

    enhance name with 2d

--- hello-dict.py
+++ hello-dict.py
@@ -1,14 +1,37 @@
-moke_beast = {
-  "name": None,
-  "type": None,
-  "special move": None,
-  "starting HP": None,
-  "starting MP": None
-}
+moke_beast = {}
 
-for name, value in moke_beast.items():
-  moke_beast[name] = input(f"Input your beast's {name} >  ").strip().title()
 
-print(
-  f"Your beast is called {moke_beast['name']}. It is an {moke_beast['type']} beast with a special move of {moke_beast['special move']}"
-)
+def prettyPrint():
+  print()
+
+  for key, value in moke_beast.items():
+    # moves along every 'key:subDictionary' pair and outputs the key (the name of the character).
+    print(key, end=": ")
+    for subKey, subValue in value.items():
+      # (nested) `for` loop moves along every subkey and subvalue in each subDictionary.
+      print(subKey, subValue, end=" | ")
+    print()
+
+
+while True:
+  name = input("Input your beast's name >  ").strip().title()
+  moke_beast_details = {
+    "type": None,
+    "special move": None,
+    "starting HP": None,
+    "starting MP": None
+  }
+  moke_beast_details["type"] = input(
+    "Input your beast's type >  ").strip().title()
+  moke_beast_details["special move"] = input(
+    "Input your beast's special move >  ").strip().title()
+  moke_beast_details["starting HP"] = input(
+    "Input your beast's starting HP >  ").strip().title()
+  moke_beast_details["starting MP"] = input(
+    "Input your beast's starting MP >  ").strip().title()
+
+  moke_beast[name] = moke_beast_details
+  prettyPrint()
+  again = input("Again? y/n > ").strip().lower()
+  if again == "n":
+    break
